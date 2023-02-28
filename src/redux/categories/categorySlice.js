8a76241cc6a73a756aa5dc952638877382e98a43
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-02-28 14:11:50 +0300

    - Install react-redux @redux/toolkit - Create a redux directory - Create book and category slices

---
+++ src/redux/categories/categorySlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+};
+
+const categorySlice = createSlice({
+  name: 'category',
+  initialState,
+  reducers: {
+    checkStatus: () => ({ categories: 'Under construction' }),
+  },
+});
+
+const categoryReducer = categorySlice.reducer;
+
+const { checkStatus } = categorySlice.actions;
+
+export { categoryReducer, checkStatus };
